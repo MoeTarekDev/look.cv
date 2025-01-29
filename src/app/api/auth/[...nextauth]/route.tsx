546cc5f83d3c1d8db0062a6e794d4bf163060e0f
Mohamed Tarek <moetarekdev@gmail.com>
2025-01-29 18:39:30 +0200

    fixing build errors

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,4 +1,5 @@
 export { GET } from "../../../_lib/auth";
-export const POST = async () => {
-  return new Response(null, { status: 200 });
-};
+// export const POST = async (req: any) => {
+//   const session = await auth();
+//   return new Response(null, { status: 200 });
+// };
